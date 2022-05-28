b123a3bf9c301a9bea258b9f529a518786c62d3d
cesar <rasecmh0@gmail.com>
2022-05-28 04:20:14 -0300

    consertado problema com os tamanhos do character e do block

--- chrome-game/script.js
+++ chrome-game/script.js
@@ -10,20 +10,12 @@ const jump = () => {
     character.classList.remove('animate');
   }, 500);
 };
-const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-const blockTop = parseInt(window.getComputedStyle(block).getPropertyValue('top'));
-const blockWidth = parseInt(window.getComputedStyle(block).getPropertyValue('width'));
-console.log(characterTop);
-console.log(blockLeft);
-console.log(blockTop);
-console.log(blockWidth);
 
 const checkDead = setInterval(() => {
 const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
 const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
 
-if(blockLeft < 44 && blockLeft > -44 && characterTop >= 104){
+if(blockLeft < 20 && blockLeft > -20 && characterTop >= 130){
   block.style.animation = 'none';
   alert('Game Over. score: '+Math.floor(counter/100));
   counter=0;
